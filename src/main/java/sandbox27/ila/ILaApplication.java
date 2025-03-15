c9cb57ac32dc06209a0ea9a04f420c4a8de3f3e6
glaures <guido@laures.de>
2025-03-15 09:49:12 +0100

    initial commit of boilerplate

---
+++ src/main/java/sandbox27/ila/ILaApplication.java
@@ -0,0 +1,13 @@
+package sandbox27.ila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sandbox27/ila/ILaApplicationTests.java
@@ -0,0 +1,13 @@
+package sandbox27.ila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
