688431d5b44694bdf9dd50546636d25f6ba8440a
Guido Laures <guido@laures.de>
2025-08-21 09:36:30 +0200

    Physik 6. Klasse

--- src/main/java/sandbox27/ila/tools/special_2526_1/Special2526_1Runner.java
+++ src/main/java/sandbox27/ila/tools/special_2526_1/Special2526_1Runner.java
@@ -1,4 +1,4 @@
-package sandbox27.ila.tools.special_2526_1.theater;
+package sandbox27.ila.tools.special_2526_1;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -6,15 +6,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import sandbox27.ila.backend.period.Period;
-import sandbox27.ila.backend.period.PeriodRepository;
-import sandbox27.ila.tools.CourseImporter;
-import sandbox27.ila.tools.CourseUserAssignmentImporter;
-import sandbox27.ila.tools.PlaceholderCheck;
-import sandbox27.ila.tools.UserImporter;
+import sandbox27.ila.tools.special_2526_1.six_grade_physics.SixGradePhysicsImporter;
+import sandbox27.ila.tools.special_2526_1.theater.TheaterBelegung;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 @Component
 @RequiredArgsConstructor
@@ -22,12 +17,15 @@ import java.time.LocalDate;
 public class Special2526_1Runner {
 
     final TheaterBelegung theaterBelegung;;
+    final SixGradePhysicsImporter sixGradePhysicsImporter;
 
     @EventListener(ApplicationReadyEvent.class)
     @Transactional
     public void onApplicationReady() throws IOException {
         log.info("Starting Theaterbelegung");
         theaterBelegung.runImport();
+        log.info("Starting Physik 6. Klasse");
+        sixGradePhysicsImporter.runImport();
         log.info("Specials finished");
     }
 
